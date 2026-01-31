e5c270f3c671a88f5e318baabaa3a29435c725c5
vivekbhogadi369-glitch <vivekbhogadi369@gmail.com>
2026-02-01 03:58:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ PastPulse Bot is working!")
+    await update.message.reply_text("✅ Bot is working perfectly!")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
-    print("Bot running...")
+    print("Bot started...")
     app.run_polling()
 
 if __name__ == "__main__":
